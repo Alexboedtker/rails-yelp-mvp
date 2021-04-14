5545ce45d3aa7c6656c91661530a6aafd3df7fe7
Alexandra Boedtker <alexboedtker@gmail.com>
2021-04-14 13:03:01 +0200

    basic styling done and routes, methods work

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,10 +1,28 @@
 class RestaurantsController < ApplicationController
+  before_action :find_restaurant, only: [:show]
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.create(restaurant_params)
+    redirect_to restaurant_path(@restaurant)
   end
 
   def show
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
+  end
+
+  def find_restaurant
+    @restaurant = Restaurant.find(params[:id])
   end
 end
